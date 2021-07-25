6f44e4e7eebd1707eafccc55ce637c52b62076c6
DenZ <https://github.com/DZz1358/Jkaros.git>
2021-07-25 22:17:20 +0300

    first commit

---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,19 @@
+import { SharedModule } from './../../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardContainerComponent } from './components/dashboard-container/dashboard-container.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardContainerComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    SharedModule
+  ]
+})
+export class DashboardModule { }
